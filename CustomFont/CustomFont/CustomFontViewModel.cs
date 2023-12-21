91c993eb8887cda533b7904a313000c817bb8f03
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-12-21 10:38:49 +0530

    Added sample.

---
+++ CustomFont/CustomFont/CustomFontViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Maui.Buttons;
+
+namespace CustomFont
+{
+    public class CustomFontViewModel
+    {
+        private List<SfSegmentItem> segmentItems;
+        public CustomFontViewModel()
+        {
+            segmentItems = new List<SfSegmentItem>()
+            {
+                new SfSegmentItem() { ImageSource = new FontImageSource() { Glyph = "\ue700", Size = 20, FontFamily = "SegmentIcon", Color = Colors.CadetBlue}, Text = "Day"},
+                new SfSegmentItem() { ImageSource = new FontImageSource() { Glyph = "\ue701", Size = 20, FontFamily = "SegmentIcon",  Color = Colors.CadetBlue }, Text = "Week"},
+                new SfSegmentItem() { ImageSource = new FontImageSource() { Glyph = "\ue701", Size = 20, FontFamily = "SegmentIcon",  Color = Colors.CadetBlue}, Text = "Month"},
+                new SfSegmentItem() { ImageSource = new FontImageSource() { Glyph = "\ue703", Size = 20, FontFamily = "SegmentIcon",  Color = Colors.CadetBlue}, Text = "Year"},
+            };
+        }
+        public List<SfSegmentItem> SegmentItems
+        {
+            get { return segmentItems; }
+            set { segmentItems = value; }
+        }
+    }
+}
---
+++ CustomFont/CustomFont/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomFont;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
